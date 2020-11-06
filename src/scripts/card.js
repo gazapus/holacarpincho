f2445bef5ddc0fcebad702d0e198fea8b9f2c013
Cristian Villafane <cristianv129@hotmail.com>
2020-11-06 16:53:42 -0300

    url de imagenes cards cambiadas

--- src/scripts/card.js
+++ src/scripts/card.js
@@ -2,7 +2,7 @@ function setCardImage() {
     let cardImages = document.getElementsByClassName("card__image");
     if (cardImages) {
         for (let cardImage of cardImages) {
-            cardImage.style.backgroundImage = `url('../src/images/${cardImage.dataset.imageurl}')`;
+            cardImage.style.backgroundImage = `url('${cardImage.dataset.imageurl}')`;
         }
     }
 }
